36954b7cd79d4fcd413e4074c3b1e36d47ba254e
Atharv190 <atharvmarathe5@gmail.com>
2026-07-04 20:14:39 +0530

    Initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,33 +1,45 @@
-import authRoutes from "./routes/authRoutes.js";
-import stationRoutes from "./routes/stationRoutes.js";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/db.js";
 
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import stationRoutes from "./routes/stationRoutes.js";
 
 dotenv.config();
-
 
 const app = express();
 
 connectDB();
 
-// Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173", 
+      process.env.FRONTEND_URL
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/stations", stationRoutes);
 
-
-// Test Route
 app.get("/", (req, res) => {
-    res.send("🚀 EV Charging Station API is Running...");
+  res.send(" EV Charging Station API is Running...");
 });
 
-// Port
-const PORT = 5000;
+app.use((req, res) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
